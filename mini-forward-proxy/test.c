2b130677dd46da5b9f653e5ee8b4d8b7d41a2bae
grapeinApril <568566487@qq.com>
2025-06-21 00:14:23 +0800

    update

---
+++ mini-forward-proxy/test.c
@@ -0,0 +1,9 @@
+//
+// Created by A on 25-5-10.
+//
+
+#include "test.h"
+
+int main(void) {
+    int test;
+}
---
+++ mini-forward-proxy/test.h
@@ -0,0 +1,8 @@
+//
+// Created by A on 25-5-10.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#endif //TEST_H
